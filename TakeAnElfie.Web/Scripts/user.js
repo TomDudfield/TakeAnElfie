4668863e0f65bb659ac5585c606115f5ed0194fe
Ralph Saunders <ralphcgsaunders@gmail.com>
2014-12-06 14:10:50 +0000

    Prototype javascript

--- TakeAnElfie.Web/Scripts/user.js
+++ TakeAnElfie.Web/Scripts/user.js
@@ -1,1 +1,12 @@
-﻿alert('beep boop')
+﻿// What connection string do i need
+var connection = $.connection.elfieHub;
+
+connection.hub.start().done(function() {
+    console.log('Connection started');
+})
+
+$(document).ready(function() {
+    $('.take-elfie').on('click', function() {
+        connection.server.send('takeElfie');
+    });
+});
