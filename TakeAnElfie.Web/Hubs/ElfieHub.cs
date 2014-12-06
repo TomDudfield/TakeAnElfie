1c763050e5fe0074538c641d0c65032d20df1102
Tom Dudfield <tomdudfield@redweb.com>
2014-12-06 14:08:35 +0000

    add hub

---
+++ TakeAnElfie.Web/Hubs/ElfieHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace TakeAnElfie.Web.Hubs
+{
+    [HubName("ElfieHub")]
+    public class ElfieHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.Caller.showMessage("thanks for: " + message);
+        }
+    }
+}
