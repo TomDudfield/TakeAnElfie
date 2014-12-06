6e0f4bde5f1d99c2417beddc264ca89b6bc56a82
Tom Dudfield <tomdudfield@redweb.com>
2014-12-06 13:21:09 +0000

    add signalr

--- TakeAnElfie.Web/Controllers/ElfieController.cs
+++ TakeAnElfie.Web/Controllers/ElfieController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace TakeAnElfie.Web.Controllers
 {
-    public class HomeController : Controller
+    public class ElfieController : Controller
     {
         public ActionResult Index()
         {
