fe014d7bb0f4c02070b0f2638da45e553f7ea35f
lguppy <luke@guppycreative.com>
2014-12-06 14:58:01 +0000

    camera script build

--- TakeAnElfie.Web/Scripts/camera.js
+++ TakeAnElfie.Web/Scripts/camera.js
@@ -9,6 +9,7 @@
 				var video = document.getElementById("v");
 				var canvas = document.getElementById("c");
 				var button = document.getElementById("b");
+				var captured = document.getElementById("i");
 
 				if(window.URL){
 					video.src = window.URL.createObjectURL(stream);
@@ -23,6 +24,7 @@
 				button.onclick = function() {
 					canvas.getContext("2d").drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
 					var img = canvas.toDataURL("image/png");
+					captured.src = img;
 					debugger;	
 				};
 				}, function(err) { alert("there was an error " + err)});
