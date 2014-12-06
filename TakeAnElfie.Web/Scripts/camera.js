798625ac77daacf0526fd16ffd8fe16c7e07335b
lguppy <luke@guppycreative.com>
2014-12-06 14:20:13 +0000

    camera script build

--- TakeAnElfie.Web/Scripts/camera.js
+++ TakeAnElfie.Web/Scripts/camera.js
@@ -1,8 +1,29 @@
 ﻿
 (function(){
-	var setupCamera = function(){
-		var deviceMedia = navigator.getUserMedia();
-		console.log(deviceMedia);
+	var setup = function(){
+		if (navigator.getUserMedia) {
+		   navigator.getUserMedia (
+
+		      // constraints
+		      {
+		         video: true,
+		         audio: false
+		      },
+
+		      // successCallback
+		      function(localMediaStream) {
+		         console.log(localMediaStream);
+		         // Do something with the video here, e.g. video.play()
+		      },
+
+		      // errorCallback
+		      function(err) {
+		         console.log("The following error occured: " + err);
+		      }
+		   );
+		} else {
+		   console.log("getUserMedia not supported");
+		}
 	}();
 
 	var takePhoto = function(){
