f4280827464e9a04ba39ad3d4cb8c8898aa2da74
Tom Dudfield <tomdudfield@redweb.com>
2014-12-09 16:55:11 +0000

    remove azure key

--- TakeAnElfie.Web/Hubs/ElfieHub.cs
+++ TakeAnElfie.Web/Hubs/ElfieHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Drawing;
 using System.IO;
 using System.Net;
@@ -6,7 +7,7 @@ using System.Text.RegularExpressions;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
-using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Tweetinvi;
 
@@ -17,6 +18,7 @@ namespace TakeAnElfie.Web.Hubs
     {
         private const string CameraGroup = "camera";
         private const string ContainerUrl = "https://takeanelfie.blob.core.windows.net/";
+        private const string ContainerName = "takeanelfie";
         private const string CdnUrl = "http://az697179.vo.msecnd.net/";
 
         public void ConnectCamera()
@@ -31,57 +33,87 @@ namespace TakeAnElfie.Web.Hubs
 
         public void ProcessImage(string userId, string image)
         {
-            const string containerFolder = "originals/";
+            const string containerName = "originals";
             var base64Data = Regex.Match(image, @"data:image/(?<type>.+?),(?<data>.+)").Groups["data"].Value;
             var imageBytes = Convert.FromBase64String(base64Data);
             var imageName = userId + ".png";
 
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["azureBlobStorage"].ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            //StorageCredentials credentials = new StorageCredentials("takeanelfie", "u3ihGBt89nJdjuVSbGI3I8Ggu5ff80RkuItFvCL1GRI5f46Yx4fQNYvdxofqUdBqamYbPUtT9Yx7nq5QXVJqOA==");
+            // CloudBlobContainer container = new CloudBlobContainer(new Uri(ContainerUrl + containerFolder), credentials);
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExists();
+            CloudBlockBlob blob = container.GetBlockBlobReference(imageName);
+
             using (MemoryStream memoryStream = new MemoryStream(imageBytes))
             {
                 memoryStream.Seek(0, SeekOrigin.Begin);
-
-                StorageCredentials credentials = new StorageCredentials("takeanelfie", "u3ihGBt89nJdjuVSbGI3I8Ggu5ff80RkuItFvCL1GRI5f46Yx4fQNYvdxofqUdBqamYbPUtT9Yx7nq5QXVJqOA==");
-                CloudBlobContainer container = new CloudBlobContainer(new Uri(ContainerUrl + containerFolder), credentials);
-                CloudBlockBlob blob = container.GetBlockBlobReference(imageName);
                 blob.UploadFromStream(memoryStream);
-            }
 
-            Clients.Client(userId).reviewImage(CdnUrl + containerFolder + imageName);
+#if DEBUG
+                Clients.Client(userId).reviewImage(blob.Uri);
+#else
+                Clients.Client(userId).reviewImage(CdnUrl + containerName + "/" + imageName);
+#endif
+            }
         }
 
         public void ApproveImage(string image)
         {
-            const string containerFolder = "processed/";
+            const string containerName = "processed";
 
             WebRequest request = WebRequest.Create(image);
             WebResponse response = request.GetResponse();
-            var responseStream = response.GetResponseStream();
-            Bitmap originalBitmap = new Bitmap(responseStream);
+            Bitmap originalBitmap = null;
+
+            using (var responseStream = response.GetResponseStream())
+            {
+                if (responseStream != null)
+                {
+                    originalBitmap = new Bitmap(responseStream);
+                }
+            }
+
+            if (originalBitmap == null)
+                return;;
 
             Bitmap overlayBitmap = new Bitmap(HttpContext.Current.Server.MapPath("~/Content/Images/assets/hat 1.png"));
-            var imageName = Context.ConnectionId + ".png";
             Graphics combinedGraphic = Graphics.FromImage(originalBitmap);
             combinedGraphic.DrawImage(overlayBitmap, 0, 0, overlayBitmap.Width, overlayBitmap.Height);
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["azureBlobStorage"].ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            //StorageCredentials credentials = new StorageCredentials("takeanelfie", "u3ihGBt89nJdjuVSbGI3I8Ggu5ff80RkuItFvCL1GRI5f46Yx4fQNYvdxofqUdBqamYbPUtT9Yx7nq5QXVJqOA==");
+            //CloudBlobContainer container = new CloudBlobContainer(new Uri(ContainerUrl + containerFolder), credentials);
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExists();
+
+            var imageName = Context.ConnectionId + ".png";
+            CloudBlockBlob blob = container.GetBlockBlobReference(imageName);
+            string imageUrl;
 
             using (MemoryStream memoryStream = new MemoryStream())
             {
                 originalBitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
                 memoryStream.Seek(0, SeekOrigin.Begin);
-
-                StorageCredentials credentials = new StorageCredentials("takeanelfie", "u3ihGBt89nJdjuVSbGI3I8Ggu5ff80RkuItFvCL1GRI5f46Yx4fQNYvdxofqUdBqamYbPUtT9Yx7nq5QXVJqOA==");
-                CloudBlobContainer container = new CloudBlobContainer(new Uri(ContainerUrl + containerFolder), credentials);
-                CloudBlockBlob blob = container.GetBlockBlobReference(imageName);
                 blob.UploadFromStream(memoryStream);
+
+#if DEBUG
+                imageUrl = blob.Uri.ToString();
+#else
+                imageUrl = CdnUrl + containerName + "/" + imageName;
+#endif
             }
 
             var twitterCredentials = TwitterCredentials.CreateCredentials("2834910083-hQCWvhAmnArzAxc80paU9ftNWtfeMaeGyHWvPzP", "3ZxjrZUCju54cBQcbp6kDE1gS6uFzAPf37kNFDAzF9WUl", "iGyrQI7U1Y8SjuXmuwFy78fZa", "L3prxTxJ2kjWlFVmWmXNyzuS8XzsOHF80kPwGutDtc8NvvueFq");
             TwitterCredentials.ExecuteOperationWithCredentials(twitterCredentials, () =>
             {
-                var tweet = Tweet.CreateTweet(CdnUrl + containerFolder + imageName);
+                var tweet = Tweet.CreateTweet(imageUrl);
                 tweet.Publish();
             });
 
-            Clients.Caller.showTweet(CdnUrl + containerFolder + imageName);
+            Clients.Caller.showTweet(imageUrl);
         }
     }
 }
